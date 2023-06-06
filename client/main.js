c3791e67f83829c688f291d1120ef2a3d1aad7c3
Diana Liu <mata7890@sina.com>
2023-06-06 12:37:54 +1000

    Structure rearranged: axios callback isolated into utils.js

--- client/main.js
+++ client/main.js
@@ -1,34 +1,7 @@
 import axios from 'axios';
-
-import {
-  customerTableBody
-} from './src/js/common';
+import { getCustomers } from './src/js/utils';
 
 
 
 const url = 'http://localhost/api/customers';
-axios.get(url).then((response)=>{
-  console.log('Frontend axios GET method.');
-  const customers = response.data.data;
-  customers.forEach((element,index)=>{
-    const tr = document.createElement('tr');
-    const th = document.createElement('th');
-    const customerName = document.createElement('td');
-    const contactName = document.createElement('td');
-    const address = document.createElement('td');
-    const city = document.createElement('td');
-    const postcode = document.createElement('td');
-    const country = document.createElement('td');
-
-    th.textContent = element.CustomerID;
-    customerName.textContent = element.CustomerName;
-    contactName.textContent = element.ContactName;
-    address.textContent = element.Address;
-    city.textContent = element.City;
-    postcode.textContent = element.Postcode;
-    country.textContent = element.Country;
-
-    tr.append(th,customerName,contactName,address,city,postcode,country);
-    customerTableBody.appendChild(tr);
-  })
-})
+axios.get(url).then(getCustomers);
--- client/src/js/common.js
+++ client/src/js/common.js
@@ -2,4 +2,3 @@ export const customerTableContainer = document.querySelector('#customerTableCont
 export const customerTable = document.querySelector('#customerTable');
 export const customerTableHead = document.querySelector('#customerTable thead');
 export const customerTableBody = document.querySelector('#customerTable tbody');
-customerTableBody.innerHTML='';

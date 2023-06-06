5d5c1d66611147947eed6af2c6232e561292c12b
Diana Liu <mata7890@sina.com>
2023-06-06 12:31:12 +1000

    Frontend axios get callback function done. The whold table is shown.

--- client/main.js
+++ client/main.js
@@ -1,15 +1,34 @@
 import axios from 'axios';
 
-import {app} from './src/js/common';
+import {
+  customerTableBody
+} from './src/js/common';
 
-const customerListContainer = document.createElement('p');
-
-app.textContent='app';
 
 
 const url = 'http://localhost/api/customers';
 axios.get(url).then((response)=>{
-  console.log('get');
-  app.append(customerListContainer);
-  customerListContainer.textContent = response.data.data[0].CustomerID;
+  console.log('Frontend axios GET method.');
+  const customers = response.data.data;
+  customers.forEach((element,index)=>{
+    const tr = document.createElement('tr');
+    const th = document.createElement('th');
+    const customerName = document.createElement('td');
+    const contactName = document.createElement('td');
+    const address = document.createElement('td');
+    const city = document.createElement('td');
+    const postcode = document.createElement('td');
+    const country = document.createElement('td');
+
+    th.textContent = element.CustomerID;
+    customerName.textContent = element.CustomerName;
+    contactName.textContent = element.ContactName;
+    address.textContent = element.Address;
+    city.textContent = element.City;
+    postcode.textContent = element.Postcode;
+    country.textContent = element.Country;
+
+    tr.append(th,customerName,contactName,address,city,postcode,country);
+    customerTableBody.appendChild(tr);
+  })
 })
--- client/src/js/common.js
+++ client/src/js/common.js
@@ -1,1 +1,5 @@
-export const app = document.querySelector('#app');
+export const customerTableContainer = document.querySelector('#customerTableContainer');
+export const customerTable = document.querySelector('#customerTable');
+export const customerTableHead = document.querySelector('#customerTable thead');
+export const customerTableBody = document.querySelector('#customerTable tbody');
+customerTableBody.innerHTML='';

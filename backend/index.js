416f4c3145f6fe700af5e7690632bac9e7c51d3b
Diana Liu <mata7890@sina.com>
2023-06-06 13:17:01 +1000

    Add express.json middleware to read request.body

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const router = require('./routes/router.js');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use('/api',router);
 
 app.listen(80,()=>{
--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,11 +3,10 @@ const database = require('../database');
 
 const router = Router();
 
-console.log('lala');
 //get customer list
 //http://localhost/api/customers
 router.get('/customers', (req, res) => {
-    console.log('Enter Customer list get method!');
+    console.log('Backend enter customers list get method!');
     try {
         var sql = 'SELECT * FROM customers';
         database.query(sql, (err, result) => {
@@ -21,5 +20,21 @@ router.get('/customers', (req, res) => {
     }
 
 });
+
+//post a new customer
+//http://localhost:5173?customerName=OluPhotography&contactName=DianaLiu&address=39ChesterRd&city=Brisbane&postcode=4113&country=AU
+//http://localhost/api/customer
+router.post('/customer',(req,res)=>{
+    try {
+        console.log('Bckend enter new customer post method');
+        console.log(req.body);
+        res.status(200).json({
+            msg:'New customer post succeed!'
+        })
+    } catch (error) {
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;
